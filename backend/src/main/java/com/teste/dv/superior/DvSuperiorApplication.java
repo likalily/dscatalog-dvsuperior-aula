bce7575989dd2db1e3d7f9236b97c792dc995cc4
Eliane Leite Andrade <elianeleiteandrade@yahoo.com.br>
2022-02-10 13:47:02 -0300

    Backend project created

---
+++ backend/src/main/java/com/teste/dv/superior/DvSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.teste.dv.superior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvSuperiorApplication.class, args);
+	}
+
+}
